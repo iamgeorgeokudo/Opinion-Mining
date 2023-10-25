10e86e200e2d92dc61c05d50d1eb749af84b9340
iamgeorgeokudo <okudog1@gmail.com>
2023-10-25 10:53:19 +0300

    Initial

---
+++ app/models.py
@@ -0,0 +1,40 @@
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask import current_app, url_for
+from . import db
+from flask_login import UserMixin
+from datetime import datetime
+from datetime import date
+
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), unique=True, nullable=False)
+    email = db.Column(db.String(60), unique=True, nullable=False)
+    password = db.Column(db.String(60), nullable=False)
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+
+    @property
+    def password(self):
+        raise AttributeError('Password is not a readable')
+
+    @password.setter
+    def password(self, password):
+        self.password = generate_password_hash(password)
+
+    def verify_password(self, password):
+        return check_password_hash(self.password, password)
+
+
+class Role(db.Model):
+    __tablename__ = 'roles'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), unique=True, nullable=False)
+    db.relationship('User', backref='role', lazy='dynamic')
+
+    def __repr__(self):
+        return '<Role %r>' % self.name
+
---
+++ application.py
@@ -0,0 +1,47 @@
+import os
+from app import create_app, db
+from flask_migrate import Migrate
+from flask import render_template
+from flask.cli import AppGroup
+from app.models import User, Role
+
+app = create_app(os.getenv('OPINIONMINING_CONFIG') or 'default')
+migrate = Migrate(app, db)
+admin_cli = AppGroup('admin')
+
+
+@app.shell_context_processor
+def make_shell_context():
+    return dict(db=db, User=User, Role=Role)
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html')
+
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html')
+
+
+@admin_cli.command('create')
+def create_admin():
+    from werkzeug.security import generate_password_hash
+    admin_role = Role.query.filter_by(name='Admin').first()
+    if not admin_role:
+        admin_role = Role(name='Admin')
+        db.session.add(admin_role)
+        db.session.commit()
+    username = input('Enter username: ')
+    email = input('Enter email: ')
+    password = input('Enter password: ')
+
+    # create admin user
+    admin_user = User(username=username, email=email, password=generate_password_hash(password), role=admin_role)
+    db.session.add(admin_user)
+    db.session.commit()
+    print('Admin created successfully')
+
+
+app.cli.add_command(admin_cli)
